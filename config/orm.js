b2727c989d9c2a092b21d381efea3cc29145a5de
Bryan Castleman <Bryan Castleman>
2017-05-04 23:35:17 -0700

    adding renamed controller js file and got a h1 tag to render on the root path

--- config/orm.js
+++ config/orm.js
@@ -7,7 +7,20 @@ var connection = require("../config/connection.js");
 
 // updateOne()
 
-var orm = {};
+var orm = {
+	all: function(tableInput, cb) {
+	    var queryString = "SELECT * FROM " + tableInput + ";";
+	    connection.query(queryString, function(err, result) {
+	      if (err) {
+	        throw err;
+	      }
+	      cb(result);
+	    });
+	},
+	
+
+
+};
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
