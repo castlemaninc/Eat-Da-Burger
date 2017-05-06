d4b9df4b405493c5416956fe667dded96b7d2670
Bryan Castleman <Bryan Castleman>
2017-05-05 23:56:35 -0700

    adding changes to forms in index.handlebars and updated the README.md

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,5 +15,35 @@ router.get("/", function(req, res){
 		res.render("index", hbsObject);
 	});
 });
+
+router.post("/", function(req, res) {
+	 burger.create([
+    "name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    condition: req.body.condition
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  cat.delete(condition, function() {
+    res.redirect("/");
+  });
+});
 // Export routes for server.js to use.
 module.exports = router;
